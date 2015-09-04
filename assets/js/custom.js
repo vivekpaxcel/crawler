39029eac6ad7d8c02fd3a5d84b431bf3e7cadbe2
vivekpaxcel <vivek.kumar@paxcel.net>
2015-09-04 18:57:14 +0530

    promotion_add.html, keywords.html,track.html,delisted.html, cat-rank.html

---
+++ assets/js/custom.js
@@ -0,0 +1,11 @@
+ $(document).ready(function () {
+	if ($(".topdivboxfixed").find('div.submenu').length > 0) {
+		
+		$(".inner-page #main").css('margin-top','110px')
+	}
+	else{
+		
+		$(".inner-page #main").css('margin-top','75px')
+	}
+
+});
